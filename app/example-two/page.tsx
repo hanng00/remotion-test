46bb8e3f6732b32fa719034ad3086edac48704d0
Hannes Gustafsson <hannes.gustafsson@burtcorp.com>
2024-01-18 12:39:54 +0100

    Ensure mobile compliant

--- app/example-two/page.tsx
+++ app/example-two/page.tsx
@@ -2,21 +2,23 @@
 
 import { Player } from "@remotion/player";
 import { Main } from "@/src/ExampleTwo/Main";
-import { useState } from "react";
-const { DURATION_IN_FRAMES, VIDEO_FPS, VIDEO_HEIGHT, VIDEO_WIDTH } = {
-  DURATION_IN_FRAMES: 30 * (10 + 5),
-  VIDEO_FPS: 30,
-  VIDEO_HEIGHT: 1280,
-  VIDEO_WIDTH: 720,
-};
+import { useState, useMemo } from "react";
+import {
+  MainProps,
+  defaultMainProps,
+  DURATION_IN_FRAMES,
+  VIDEO_FPS,
+  VIDEO_HEIGHT,
+  VIDEO_WIDTH,
+} from "@/src/ExampleTwo/types/constants";
+import { z } from "zod";
 
 const ExampleTwoPage = () => {
-  const [wrapperData, setWrapperData] = useState({
-    topSongName: "1989 (Taylor's Version)",
-    topSongArtistName: "Taylor Swift",
-    topSongCover:
-      "https://i.scdn.co/image/ab67616d0000b273e4b6b5e2b7b6b7b6b7b6b7b6",
-  });
+  const [wrapperData, setWrapperData] = useState(defaultMainProps);
+
+  const inputProps: z.infer<typeof MainProps> = useMemo(() => {
+    return wrapperData;
+  }, [wrapperData]);
 
   return (
     <div className="container flex flex-col h-screen">
@@ -24,8 +26,8 @@ const ExampleTwoPage = () => {
         <h1 className="text-3xl font-serif">Example Two</h1>
         <p className="max-w-md text-sm">Text</p>
       </div>
-      <div className="w-full h-full">
-        <div className="h-full w-full mx-auto shadow border rounded-2xl flex items-center justify-center">
+      <div className="flex items-center justify-center w-full h-full">
+        <div className="h-full w-fit my-8 shadow border rounded-2xl overflow-hidden">
           <Player
             component={Main}
             durationInFrames={DURATION_IN_FRAMES}
@@ -33,28 +35,56 @@ const ExampleTwoPage = () => {
             compositionHeight={VIDEO_HEIGHT}
             compositionWidth={VIDEO_WIDTH}
             style={{
-              width: "100%",
+              aspectRatio: VIDEO_WIDTH / VIDEO_HEIGHT,
+              height: "100%",
+              maxHeight: "100%",
             }}
-            inputProps={wrapperData}
+            inputProps={inputProps}
             controls
             autoPlay
             loop
           />
         </div>
       </div>
-      <div>
-        Input song title:
-        <input
-          onChange={(e) => {
-            setWrapperData({
-              ...wrapperData,
-              topSongName: e.target.value,
-            });
-          }}
-        />
-      </div>
 
-      <div className="py-4 font-bold">Made with Legacy</div>
+      <div className="flex flex-row flex-wrap py-4">
+        <div>
+          Input artist name:
+          <input
+            value={wrapperData.topSongArtistName}
+            onChange={(e) => {
+              setWrapperData({
+                ...wrapperData,
+                topSongArtistName: e.target.value,
+              });
+            }}
+          />
+        </div>
+        <div>
+          Input song:
+          <input
+            value={wrapperData.topSongName}
+            onChange={(e) => {
+              setWrapperData({
+                ...wrapperData,
+                topSongName: e.target.value,
+              });
+            }}
+          />
+        </div>
+        <div>
+          Input album source:
+          <input
+            value={wrapperData.topSongCover}
+            onChange={(e) => {
+              setWrapperData({
+                ...wrapperData,
+                topSongCover: e.target.value,
+              });
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- src/ExampleTwo/types/constants.ts
+++ src/ExampleTwo/types/constants.ts
@@ -6,18 +6,27 @@ export const SCENE_1_DURATION = 9 * VIDEO_FPS;
 export const SCENE_2_DURATION = 9 * VIDEO_FPS;
 export const SCENE_3_DURATION = 7 * VIDEO_FPS;
 
+export const Scene2Props = z.object({})
+export const defaultScene2Props: z.infer<typeof Scene2Props> = {}
+
 export const Scene3Props = z.object({
   topSongName: z.string(),
   topSongArtistName: z.string(),
   topSongCover: z.string(),
 });
-
 export const defaultScene3Props: z.infer<typeof Scene3Props> = {
   topSongName: "1989 (Taylor's Version)",
   topSongArtistName: "Taylor Swift",
   topSongCover:
-    "https://i.scdn.co/image/ab67616d0000b273e4b6b5e2b7b6b7b6b7b6b7b6",
+    "https://store.taylorswift.com/cdn/shop/files/1mjQym0yi2krxJWjFtvkNx0fXwYrHhkH_800x.png?v=1691644764",
 };
+
+export const MainProps = Scene2Props.merge(Scene3Props);
+export const defaultMainProps: z.infer<typeof MainProps> = {
+  ...defaultScene2Props,
+  ...defaultScene3Props,
+};
+
 
 export const VIDEO_WIDTH = 720;
 export const VIDEO_HEIGHT = 1280;
