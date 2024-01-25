1fbc7b40a8ddd7cf3ea20d329935481ee197552b
Hannes Gustafsson <hannes.gustafsson@burtcorp.com>
2024-01-25 15:31:34 +0100

    Refactor old api/chat into service

---
+++ lib/services/ChatService/chat-service.ts
@@ -0,0 +1,55 @@
+import { openai } from "@/lib/openai";
+import { functions, runServerFunction } from "./functions";
+import { OpenAIStream } from "ai";
+
+interface ChatServiceProps {
+  messages: any[];
+  model?: string;
+  max_tokens?: number;
+  onToken?: (token: string) => void;
+  onCompletion?: (completion: string) => void;
+}
+
+export const ChatService = async ({
+  messages,
+  model = "gpt-4-1106-preview",
+  max_tokens = 150,
+  onToken = () => { },
+  onCompletion = () => { }
+}: ChatServiceProps): Promise<ReadableStream<any>> => {
+  const response = await openai.chat.completions.create({
+    model,
+    stream: true,
+    max_tokens,
+    messages,
+    functions
+  });
+
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response,
+    {
+      experimental_onFunctionCall: async (
+        { name, arguments: args },
+        createFunctionCallMessages,
+      ) => {
+        console.log("onFunctionCall", name, args)
+        const result = await runServerFunction(name, args);
+        if (!result) return
+
+        const newMessages = createFunctionCallMessages(
+          result
+        );
+        console.log("newMessages", newMessages)
+        return openai.chat.completions.create({
+          messages: [...messages, ...newMessages],
+          stream: true,
+          model,
+          functions,
+        });
+      },
+      onToken,
+      onCompletion
+    });
+
+  return stream
+}
